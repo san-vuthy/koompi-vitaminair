34c41b2485b2a5bd36aaa39a65d81e9220ec0010
san-vuthy <san.vuthy08@gmail.com>
2021-02-27 10:57:52 +0700

    add ci/cd

---
+++ .github/workflows/script.sh
@@ -0,0 +1,1 @@
+rsync -a /home/runner/work/vitaminair.org/vitaminair.org/server/* ubuntu@ec2-13-229-154-169.ap-southeast-1.compute.amazonaws.com:/var/www/vitaminair.org/backend && rsync -a /home/runner/work/vitaminair.org/vitaminair.org/client/.??* ubuntu@ec2-13-229-154-169.ap-southeast-1.compute.amazonaws.com:/var/www/vitaminair.org/client
