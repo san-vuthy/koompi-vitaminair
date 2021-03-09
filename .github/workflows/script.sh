1b688240169dd1df0b07583ce1225ff3f61af925
san-vuthy <san.vuthy08@gmail.com>
2021-03-09 10:26:52 +0700

    update

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,4 +1,4 @@
 rsync -a /home/runner/work/vitaminair.org/vitaminair.org/server/* ubuntu@ec2-13-229-154-169.ap-southeast-1.compute.amazonaws.com:/var/www/demo.vitaminair.org/server && \
 rsync -a /home/runner/work/vitaminair.org/vitaminair.org/client/build/* ubuntu@ec2-13-229-154-169.ap-southeast-1.compute.amazonaws.com:/var/www/demo.vitaminair.org/client/public && \
-rsync -a /home/runner/work/vitaminair.org/vitaminair.org/client/build/* ubuntu@ec2-13-229-154-169.ap-southeast-1.compute.amazonaws.com:/var/www/demo.vitaminair.org/client/public && \
+rsync -a /home/runner/work/vitaminair.org/vitaminair.org/dashboard/build/* ubuntu@ec2-13-229-154-169.ap-southeast-1.compute.amazonaws.com:/var/www/demo.vitaminair.org/dashboard/public && \
 echo "The Production deploy completed."
